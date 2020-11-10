86c011ab1515f510e3613f6f61aec2d2fc56e71e
liy <liy@qq.com>
2020-11-10 16:06:25 +0800

    rtt

---
+++ packageMall/pages/orders/orders/index.js
@@ -0,0 +1,42 @@
+const { $Toast } = require('../../../../dist/base/index');
+import { wx_gotoNewUrl } from '../../../../utils/fn'
+//获取应用实例
+const app = getApp()
+Page({
+  data:{
+    url: app.globalData.url,
+    showMask: false,
+    visible: false,
+    tabList:[
+      { id:1, name:"全部" },
+      { id:2, name:"待付款" },
+      { id:3, name:"待发货" },
+      { id:4, name:"待收货" },
+      { id:5, name:"已收货" },
+    ],
+    currentTab:0,
+  },
+  onShow(){
+  },
+  clickTab(e){
+    let cur = e.currentTarget.dataset.current;
+    if(this.data.currentTab == cur){
+      return false;
+    }else{
+      this.setData({
+        currentTab:cur,
+      }) 
+    }
+  },
+  handleOk(){
+    this.setData({
+      visible: false
+    })
+    wx_gotoNewUrl('navigateTo','/pages/loginAll/loginAdmin/index')
+  },
+  handleClose(){
+    this.setData({
+      visible: false
+    })
+  },
+})
